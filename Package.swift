ce78056d03c669160cb436f3a2d574ef59815322
Anbarasan <anbuarasan2001@gmail.com>
2023-08-10 11:01:49 +0530

    Added displayer package dependency

--- Package.swift
+++ Package.swift
@@ -13,9 +13,7 @@ let package = Package(
         .library(name: "Modele", targets: ["Model"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/Anbarasan-S/SwiftPackageManager.git", from: "1.0.0")
+        .package(url: "https://github.com/Anbarasan-S/SwiftPackageManager.git", exact: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,6 +25,8 @@ let package = Package(
             name: "DemoPackage2Tests",
             dependencies: ["DemoPackage2"]),
         
-            .target(name: "Model")
+            .target(name: "Model",
+                dependencies: ["DisplayerPackage"]
+                )
     ]
 )
