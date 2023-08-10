32b3e26b7f1201d3ff295f1d390b2c872dbb8f61
Anbarasan <anbuarasan2001@gmail.com>
2023-08-10 10:23:05 +0530

    Added the model package

---
+++ Sources/DemoPackage2/MyTest.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by anbarasan-17476 on 09/08/23.
+//
+
+import Foundation
+
+
+public struct MyTest
+{
+    func printTest() -> String
+    {
+        return "Hello"
+    }
+}
---
+++ Sources/Model/ModelProvider.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by anbarasan-17476 on 09/08/23.
+//
+
+import Foundation
+import DisplayerPackage
+
+public struct ModelProvider
+{
+    public init()
+    {
+        debugPrint("Model Provider")
+        _ = DisplayerPackage.TestViewController()
+    }
+}
